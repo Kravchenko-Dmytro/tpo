84a2f79a39db32c6d6932c57fbd931f50d96a5d3
Kravchenko-Dmytro <63056355+Kravchenko-Dmytro@users.noreply.github.com>
2022-08-15 22:40:53 +0300

    Add files via upload

---
+++ posl_alg/src/com/company/Sort.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.*;
+
+public class Sort {
+    public ArrayList<Integer> arrayList;
+    private Integer[] array;
+    public Sort(ArrayList<Integer> array){
+        arrayList = array;
+    }
+    public void sort() {
+        try {
+            Integer key, j;
+            array = new Integer[arrayList.size()];
+            for (int f = 0; f < arrayList.size(); f++) {
+                array[f] = arrayList.get(f);
+            }
+            int n = array.length;
+            for (int i = 1; i < n; ++i) {
+                key = array[i];
+                j = i - 1;
+                while (j >= 0 && array[j] > key) {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+            arrayList = new ArrayList<Integer>();
+            for (int f = 0; f < array.length; f++) {
+                arrayList.add(array[f]);
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
